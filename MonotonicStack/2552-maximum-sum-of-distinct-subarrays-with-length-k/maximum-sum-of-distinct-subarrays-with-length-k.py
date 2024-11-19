b69fec2f1266c84cf819ee73e06968976df990a6
Aditi Anand <110238534+Aditikhasnis@users.noreply.github.com>
2024-11-19 17:30:05 +0530

    Time: 86 ms (97.08%) | Memory: 33.7 MB (82.13%) - LeetSync

--- MonotonicStack/2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
+++ MonotonicStack/2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -7,19 +7,20 @@ class Solution:
         maximum=0
         sumi=0
         for r in range(n):
-
+            
             while nums[r] in vis:
                 vis.remove(nums[l])
                 sumi-=nums[l]
                 l+=1
             vis.add(nums[r])
             sumi+=nums[r]
+            # print(f'{l}{r}{vis}')
             if (r-l+1)==k:
                 maximum=max(maximum,sumi)
                 sumi-=nums[l]
                 vis.remove(nums[l])
                 l+=1
-            
+            # print(f'{l}{r}{vis}')
             # print(f'{vis} {nums[r]} {l}')
 
         
