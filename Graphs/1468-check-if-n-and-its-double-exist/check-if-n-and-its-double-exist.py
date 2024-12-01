14178168f348b1d67da92d3ad21482b96d6574f1
Aditi Anand <110238534+Aditikhasnis@users.noreply.github.com>
2024-12-01 15:48:08 +0530

    Time: 3 ms (57.43%) | Memory: 17.7 MB (5.25%) - LeetSync

--- Graphs/1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
+++ Graphs/1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
@@ -9,8 +9,6 @@ class Solution:
         for i in range(len(arr)):
             n=arr[i]
             if n%2==0 and n//2 in vis:
-                # if i==(arr[i]//2) and c[arr[i]]>1:
-                    # print(i)
                 if arr[i]==(n//2) and c[arr[i]]==1:
                     continue
                 else:
