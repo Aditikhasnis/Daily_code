e6e66d412b7fc118a76240f283652b6e18ef05ab
Aditi Anand <110238534+Aditikhasnis@users.noreply.github.com>
2024-12-16 11:41:38 +0530

    Time: 70 ms (41.86%) | Memory: 18 MB (5.31%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -6,17 +6,12 @@ class Solution:
         n=len(nums)
         mini=0
         while j<n:
-
             if nums[j]==0:
                 temp+=1
-
             while temp>k:
                 if nums[i]==0:
                     temp-=1
                 i+=1
-
-
-
             
             mini=max(mini,j-i+1)
             # print(f'{i}-->{j}->{mini}')
