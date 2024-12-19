0237b78cab6f23b7b5abfe850e06f84b37b45d91
Aditi Anand <110238534+Aditikhasnis@users.noreply.github.com>
2024-12-19 08:13:02 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (10.80%) - LeetSync

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
@@ -0,0 +1,31 @@
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+
+
+        n=len(prices)
+        stk=[]
+        res=[-1 for i in prices]
+        k=n-1
+  
+        for i in range(n-1,-1,-1):
+
+            while stk and stk[-1]>prices[i]:
+                stk.pop()
+            if stk and stk[-1]<=prices[i]:
+                res[k]=stk[-1]
+        
+            stk.append(prices[i])
+            k-=1
+            # print(stk)
+        for i in range(n):
+
+            if res[i]!=-1:
+                res[i]=prices[i]-res[i]
+            else:
+                res[i]=prices[i]
+
+        return res
+
+
+
+        
