e10b3a8ce35b9e9bc4c6daa20a0390836ed7aeef
Aditi Anand <110238534+Aditikhasnis@users.noreply.github.com>
2025-01-08 22:06:00 +0530

    Time: 0 ms (100.00%) | Memory: 51 MB (48.94%) - LeetSync

--- 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.js
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.js
@@ -4,20 +4,16 @@
  */
 var countPrefixSuffixPairs = function(words) {
 
-    let count=0;
-    for(let i=0;i<words.length;i++)
-    {
-        word=words[i];
-        for(let j=i+1;j<words.length;j++)
-        {
-            if(word===words[j].substring(0,word.length) && word===words[j].substring(words[j].length-word.length,words[j].length))
-            {
-                count++;
-            }
+let count = 0;
 
+for (let i = 0; i < words.length; i++) {
+    for (let j = i + 1; j < words.length; j++) {
+        if (words[j].startsWith(words[i]) && words[j].endsWith(words[i])) {
+            count++;
         }
     }
+}
 
-    return count;
+return count;
     
 };
