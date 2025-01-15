1c6199342d921280c584c700dd7ea1fe718884ee
Aditi Anand <110238534+Aditikhasnis@users.noreply.github.com>
2025-01-15 20:36:04 +0530

    Time: 69 ms (9.20%) | Memory: 52.4 MB (8.88%) - LeetSync

--- 2789-counter-ii/counter-ii.js
+++ 2789-counter-ii/counter-ii.js
@@ -3,7 +3,7 @@
  * @return { increment: Function, decrement: Function, reset: Function }
  */
 var createCounter = function(init) {
-    CurrentCount=init
+    let CurrentCount=init
     function inc()
     {
         CurrentCount++;
