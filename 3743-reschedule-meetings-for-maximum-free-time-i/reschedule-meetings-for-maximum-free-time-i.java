7e6f85a6e969b0254f011f27e41cc9c77acd8570
Aditi Anand <110238534+Aditikhasnis@users.noreply.github.com>
2025-07-13 00:09:43 +0530

    Time: 546 ms (5.33%) | Memory: 66.8 MB (5.06%) - LeetSync

---
+++ 3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maxFreeTime(int eventTime, int k, int[] startTime, int[] endTime) {
+        int result=0;
+        int n=startTime.length;
+        int[] gap = new int[n+1];
+        gap[0]=startTime[0];
+        for(int i=1;i<startTime.length;i++)
+        {
+            gap[i]=startTime[i]-endTime[i-1];
+        }
+
+        gap[n]=eventTime-endTime[n-1];
+        int cnt=0;
+
+        for(int i=0;i<=k;i++)
+        {
+            cnt+=gap[i];
+        }
+        int maxi=-1;
+
+        maxi=Math.max(maxi,cnt);
+
+        for(int i=1;i<n-k+1;i++)
+        {
+            System.out.println(cnt);
+            cnt-=gap[i-1];
+            System.out.println(gap[i-1] + " " + gap[i+k]);
+            cnt+=gap[i+k];
+            maxi=Math.max(maxi,cnt);
+        }
+
+        return maxi;
+    }
+}
