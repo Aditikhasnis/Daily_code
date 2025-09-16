c0efe4f46d8bcbd82101d7ea28fda85ef404e51b
Aditi Anand <110238534+Aditikhasnis@users.noreply.github.com>
2025-09-17 00:15:57 +0530

    Time: 4 ms (26.58%) | Memory: 44.6 MB (46.02%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,31 +1,41 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashMap<Integer, HashSet<Character>> cols = new HashMap<>();
+        HashMap<Integer, HashSet<Integer> > map = new HashMap<>();
+        HashSet<Integer> seti = new HashSet<>();
+        HashSet<Integer> row = new HashSet<>();
         HashMap<Integer, HashSet<Character>> boxes = new HashMap<>();
-        
-        for (int i = 0; i < 9; i++) {
-            HashSet<Character> row = new HashSet<>();
+        for(int i=0;i<board.length;i++)
+        {
             
-            for (int j = 0; j < 9; j++) {
+            for(int j=0;j<board[i].length;j++)
+            {
+
+                
+
+                if(board[i][j]!='.')
+                {
+                  if((map.containsKey(j) && map.get(j).contains(board[i][j]-'1'))|| row.contains(board[i][j]-'1'))
+                  {
+                    return false;
+                  }
+                  
+                  if(!map.containsKey(j))
+                  {
+                    map.put(j, new HashSet<>());
+                  }
+                  map.get(j).add(board[i][j]-'1');
+                  row.add(board[i][j]-'1');
                 char c = board[i][j];
-                if (c == '.') continue;
-
-                // Row check
-                if (row.contains(c)) return false;
-                row.add(c);
-
-                // Column check
-                if (!cols.containsKey(j)) cols.put(j, new HashSet<>());
-                if (cols.get(j).contains(c)) return false;
-                cols.get(j).add(c);
-
-                // Box check
                 int boxIndex = (i / 3) * 3 + (j / 3);
                 if (!boxes.containsKey(boxIndex)) boxes.put(boxIndex, new HashSet<>());
                 if (boxes.get(boxIndex).contains(c)) return false;
                 boxes.get(boxIndex).add(c);
+                }
+                
             }
+            row.clear();
         }
+
         return true;
     }
 }
