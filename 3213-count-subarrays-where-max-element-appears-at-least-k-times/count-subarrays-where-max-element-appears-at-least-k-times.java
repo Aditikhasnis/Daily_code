2806fdae0c23a3ed569cbf9447f07d38fce7a9f0
Aditi Anand <110238534+Aditikhasnis@users.noreply.github.com>
2026-02-17 22:20:43 +0530

    Time: 136 ms (5.25%) | Memory: 126.1 MB (5.10%) - LeetSync

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.java
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.java
@@ -1,30 +1,43 @@
 class Solution {
-    public long countSubarrays(int[] nums, int k) {
-        int n = nums.length;
-        int maxNum = Arrays.stream(nums).max().getAsInt();
-        Map<Integer, Integer> map = new HashMap<>();
-        int start = 0;
-        long res = 0;
-
-        for (int end = 0; end < n; end++) {
-            map.put(nums[end], map.getOrDefault(nums[end], 0) + 1);
-
-            // ✅ Count as soon as we get at least k max elements
-            if (nums[end] == maxNum && map.get(maxNum) >= k) {
-                res += (n - end);
-            }
-
-            // ✅ Then shrink the window from the left as long as condition holds
-            while (map.getOrDefault(maxNum, 0) >= k) {
-                map.put(nums[start], map.get(nums[start]) - 1);
-
-                // If even after shrinking we still have >=k max, count again
-                if (map.get(maxNum) >= k) {
-                    res += (n - end);
-                }
-                start++;
-            }
+    public boolean windowHasMax(int max , HashMap<Integer,Integer> map , int freq)
+    {
+        if(map.containsKey(max) && map.get(max)>=freq)
+        {
+            return true;
         }
-        return res;
+        return false;
+    }
+    public long countSubarrays(int[] nums, int k) {
+        int j=0;
+        int maxi = Arrays.stream(nums).max().getAsInt();
+        HashMap<Integer,Integer> _map = new HashMap<>();
+        long count=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            _map.put(nums[i],_map.getOrDefault(nums[i],0)+1);
+            if(nums[i]==maxi && _map.get(maxi)>=k)
+            {
+                count+=nums.length -i ;
+            }
+            while(windowHasMax(maxi,_map,k))
+            {
+               
+              _map.replace(nums[j],_map.get(nums[j])-1);
+                if(_map.get(nums[j])==0)
+                {
+                    _map.remove(nums[j]);
+                }
+                if(windowHasMax(maxi,_map,k))
+                {
+                    count+=nums.length-i;
+                }
+                j++;
+                
+            }
+
+        }
+
+        return count;
+        
     }
 }
