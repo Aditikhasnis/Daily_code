ffc1cd65b4da7018f46bf76127065f992068af11
Aditi Anand <110238534+Aditikhasnis@users.noreply.github.com>
2026-03-08 22:53:40 +0530

    Time: 78 ms (16.93%) | Memory: 106.6 MB (8.54%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
@@ -7,8 +7,10 @@ class Solution {
         int max_value=1;
         for(int r=0 ; r< nums.length ;r++)
         {
+            //if key exist increase by 1 else default to 1 
             map.merge(nums[r] , 1, Integer::sum);
-         
+            //if current key is greater than k then shift the left pointer till
+            // the value at the right becomes <=k
             while(l<nums.length && map.get(nums[r]) > k )
             {
                 map.replace(nums[l],map.get(nums[l])-1);
