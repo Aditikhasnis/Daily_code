b9b728fcba819fc03c93531466d350627031cccb
Aditi Anand <110238534+Aditikhasnis@users.noreply.github.com>
2026-03-08 22:51:21 +0530

    Time: 70 ms (45.55%) | Memory: 109.9 MB (7.66%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
@@ -8,21 +8,16 @@ class Solution {
         for(int r=0 ; r< nums.length ;r++)
         {
             map.merge(nums[r] , 1, Integer::sum);
-            max_value = Math.max(max_value,map.get(nums[r]));
-            if(max_value>k)
-            { 
-               while(l<nums.length && map.get(nums[r]) > k )
-               {
-                 map.replace(nums[l],map.get(nums[l])-1);
-                 if(map.get(nums[l])==0)
-                 {
+         
+            while(l<nums.length && map.get(nums[r]) > k )
+            {
+                map.replace(nums[l],map.get(nums[l])-1);
+                if(map.get(nums[l])==0)
+                {
                     map.remove(nums[l]);
-                 }
-                 l++;
-               }
-               max_value=1;
+                }
+                l++;
             }
-
             result = Math.max(result , r-l+1);
            
         }
