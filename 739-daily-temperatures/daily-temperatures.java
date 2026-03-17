54950d11721bddbbaa6a36f79176006800cefd4c
Aditi Anand <110238534+Aditikhasnis@users.noreply.github.com>
2026-03-17 09:02:41 +0530

    Time: 73 ms (29.14%) | Memory: 107.7 MB (33.99%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] ans = new int[n];
+        Arrays.fill(ans,0);
+        Stack<Integer> stk = new Stack<>();
+        HashMap<Integer,Integer> map = new LinkedHashMap<>();
+        for(int i=0;i<n;i++)
+        {
+            while(!stk.isEmpty() && temperatures[stk.peek()] < temperatures[i])
+            {
+              
+                    
+                ans[stk.peek()]= i-stk.pop();
+
+
+            }
+            stk.push(i);
+        }
+
+        return ans;
+    }
+}
