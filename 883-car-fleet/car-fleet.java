9aa6f1e3e38531575ed219c8e7e9d4bd7eb602e1
Aditi Anand <110238534+Aditikhasnis@users.noreply.github.com>
2026-03-22 22:49:21 +0530

    Time: 314 ms (5.01%) | Memory: 108.4 MB (12.54%) - LeetSync

--- 883-car-fleet/car-fleet.java
+++ 883-car-fleet/car-fleet.java
@@ -1,8 +1,7 @@
 import java.math.BigDecimal;
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-
-         PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+              PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
 
         for( int i =0 ;i<position.length ; i++)
         {
@@ -19,17 +18,22 @@ class Solution {
         while(!stk.isEmpty())
         {
             int[] aee = stk.pop();
-double currentTime = (double)(target - aee[0]) / aee[1];
+            double currentTime = (double)(target - aee[0]) / aee[1];
 
 // Convert to BigDecimal only if you specifically need that type for storage/precision
-BigDecimal time = BigDecimal.valueOf(currentTime);
+            BigDecimal time = BigDecimal.valueOf(currentTime);
 
 // Compare using doubleValue() as you intended
-if (fleet_group.doubleValue() == 0 || fleet_group.doubleValue() < time.doubleValue()) {
-    fleet++;
-    fleet_group = time;
-}
+            if (fleet_group.doubleValue() == 0 || fleet_group.doubleValue() < time.doubleValue()) {
+                fleet++;
+                fleet_group = time;
+            }
 
+            if(fleet_group.doubleValue()==0 || fleet_group.doubleValue()<time.doubleValue())
+            {
+                fleet++;
+                fleet_group=time;
+            }
 
         }
         return fleet;
